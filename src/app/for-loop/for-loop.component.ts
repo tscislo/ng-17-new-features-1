5c6bd73eb0e2d994277bcd6b520d42d931ada8b6
Scislo Tomasz <scislot@gmail.com>
2024-04-18 10:26:22 +0200

    if-else

--- src/app/for-loop/for-loop.component.ts
+++ src/app/for-loop/for-loop.component.ts
@@ -8,9 +8,12 @@ import {NgForOf} from "@angular/common";
         NgForOf
     ],
     template: `
+        <button (click)="seedItems()">Seed</button>
         <ul>
             @for (item of items; track item.name) {
                 <li>{{ item.name }}</li>
+            } @empty {
+                <p>No items!</p>
             }
 
             <li *ngFor="let item of items; trackBy: trackBy">{{ item.name }}</li>
@@ -20,20 +23,24 @@ import {NgForOf} from "@angular/common";
     styles: ``
 })
 export class ForLoopComponent {
-    items =  [
-        {
-            name: 'item1',
-            value: 1
-        },
-        {
-            name: 'item2',
-            value: 2
-        },
-        {
-            name: 'item3',
-            value: 3
-        }
-    ]
+    items: {name: string; value: number}[] = [];
 
     trackBy = (idx: any, item: {name: string; value: number}) => item.name;
+
+    seedItems() {
+        this.items =  [
+            {
+                name: 'item1',
+                value: 1
+            },
+            {
+                name: 'item2',
+                value: 2
+            },
+            {
+                name: 'item3',
+                value: 3
+            }
+        ]
+    }
 }
---
+++ src/app/if-else/if-else.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-if-else',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      if-else works!
+    </p>
+  `,
+  styles: ``
+})
+export class IfElseComponent {
+
+}
